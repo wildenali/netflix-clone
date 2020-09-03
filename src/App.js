3ee4e99cdc8e964bbc3e3760ef3187d86a025896
wildenali <wildenwildenaliali@gmail.com>
2020-09-03 13:24:01 +0700

    passing the statement for isLargeRow, biar gampang bacanya

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
+        isLargeRow={true}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
