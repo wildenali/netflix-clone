a048cf9e5e61daea318ccd2e7ef247d417ef1777
wildenali <wildenwildenaliali@gmail.com>
2020-09-03 10:08:51 +0700

    Membuat Rows.js file dan mengirimkan props dari App.js ke Row.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone</h1>
+      <Row title="NETFLIX ORIGINALS" />
+      <Row title="Trending Now" />
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Row({ title }) {
+  return (
+    <div>
+      {/* title */}
+      <h2>{title}</h2>
+
+      {/* Container -> posters */}
+    </div>
+  )
+}
+
+export default Row
