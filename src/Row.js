56c6a9f82cd128389a5303671bc65a2df7e21b5b
wildenali <wildenwildenaliali@gmail.com>
2020-09-03 11:24:31 +0700

    Cuma rapihin aja pake pretier

--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import axios from './axios'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
-
   const [movies, setMovies] = useState([]);
 
   // Asnippet of code which runs based on a specific condition/variable
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(fetchUrl)
+      const request = await axios.get(fetchUrl);
       // console.log(request);
       setMovies(request.data.results);
       return request;
@@ -29,14 +28,16 @@ function Row({ title, fetchUrl }) {
       {/* Container -> posters */}
       <div className="row__posters">
         {/* several row_poster(s) */}
-        {
-          movies.map(movie => (
-            <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
-          ))
-        }
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
