02cc442dd7f2993149a67161d59b1ef4395c0e2c
wildenali <wildenwildenaliali@gmail.com>
2020-09-03 10:52:25 +0700

    READ data from API TMDb, dan tampilkan di console

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from './axios'
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
+
+  const [movies, setMovies] = useState([]);
+
+  // Asnippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl)
+      // console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]); // if [], run once whe the row loads, and dont run again
+
+  console.log(movies);
+
   return (
     <div>
       {/* title */}
